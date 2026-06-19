93d36a992aa61d9a357a288469a2ac70fb06c351
Sagar Raut <2sagarraut2@gmail.com>
2026-06-19 10:47:02 +0530

    upload 03 completed

---
+++ MyProjects/03-C_Assignments/RTR_C_Snippets_Upload_03_06062026/08-TypeConversion/TypeConversion-C.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // variable declarations
+    int i, j;
+    char ch_01, ch_02;
+
+    int a, result_int;
+    float f, result_float;
+
+    int i_explicit;
+    float f_explicit;
+
+    // code
+    printf("\n\n");
+
+    // INTERCONVERSION AND IMPLICIT TYPE-CASTING BETWEEN 'char' AND 'int' Types...
+    i = 70;
+    ch_01 = i;
+    printf("I = %d\n", i);
+    printf("Character 1 (after ch_01 = i) = %c\n\n", ch_01);
+
+    ch_02 = 'Q';
+    j = ch_02;
+    printf("Character 2 = %c\n", ch_02);
+    printf("J (after j = ch_02) = %d\n\n", j);
+
+    // IMPLICIT CONVERSION OF 'int' TO 'float'...
+    a = 5;
+    f = 7.8f;
+    result_float = a + f;
+    printf("Integer a = %d and floating point nunber %f added gives floating point sum = %f\n\n", a, f, result_float);
+
+    result_int = a + f;
+    printf("Integer a = %d and floating point nunber %f added gives integer Sum = %d\n\n", a, f, result_int);
+
+    // EXPLICT TYPE CASTING
+    f_explicit = 30.121995f;
+    i_explicit = (int)f_explicit;
+    printf("Floating point number which will be type casted explicit = %f\n", f_explicit);
+
+    printf("Resultant integer after Explicit type casting of %f = %d\n\n", f_explicit, i_explicit);
+
+    return 0;
+}
