6a819499cf16140da18bca049181b9e245b8b72b
Sagar Raut <2sagarraut2@gmail.com>
2026-08-16 22:08:45 +0530

    Initials added to variable names

--- MyProjects/01-OpenGL/01-FFP/01-Windows/01-Windowing/01-Window/Window.c
+++ MyProjects/01-OpenGL/01-FFP/01-Windows/01-Windowing/01-Window/Window.c
@@ -8,42 +8,41 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
 {
 	// variable declarations
-	WNDCLASSEX wndclass;
+	WNDCLASSEX SR_wndclass;
 	HWND hwnd = NULL;
 	MSG msg;
 	TCHAR lpszAppName[] = TEXT("RTR7_SSR");
 
 	// code
 	// WNDCLASSEX structure initialisation
-	wndclass.cbSize = sizeof(WNDCLASSEX);
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.hInstance = hInstance;
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.lpszClassName = lpszAppName;
-	wndclass.lpszMenuName = NULL;
-	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	SR_wndclass.cbSize = sizeof(WNDCLASSEX);
+	SR_wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	SR_wndclass.cbClsExtra = 0;
+	SR_wndclass.cbWndExtra = 0;
+	SR_wndclass.lpfnWndProc = WndProc;
+	SR_wndclass.hInstance = hInstance;
+	SR_wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	SR_wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	SR_wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	SR_wndclass.lpszClassName = lpszAppName;
+	SR_wndclass.lpszMenuName = NULL;
+	SR_wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
 	// Register above WNDCLASS structure
-	RegisterClassEx(&wndclass);
+	RegisterClassEx(&SR_wndclass);
 
 	// create the window
 	hwnd = CreateWindow(lpszAppName,
-		TEXT("My First RTR7 Program : Sagar Sambhaji Raut"),
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-	);
+						TEXT("My First RTR7 Program : Sagar Sambhaji Raut"),
+						WS_OVERLAPPEDWINDOW,
+						CW_USEDEFAULT,
+						CW_USEDEFAULT,
+						CW_USEDEFAULT,
+						CW_USEDEFAULT,
+						NULL,
+						NULL,
+						hInstance,
+						NULL);
 
 	// show window
 	ShowWindow(hwnd, iCmdShow);
@@ -58,7 +57,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 		DispatchMessage(&msg);
 	}
 
-	return((int)msg.wParam);
+	return ((int)msg.wParam);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
@@ -69,9 +68,18 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+	case WM_KEYDOWN:
+		PostQuitMessage(0);
+		break;
+	case WM_RBUTTONDOWN:
+		PostQuitMessage(0);
+		break;
+	case WM_LBUTTONDOWN:
+		PostQuitMessage(0);
+		break;
 	default:
 		break;
 	}
 
-	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
